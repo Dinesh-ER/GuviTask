a2410d14cdd4809a8409f855405ee9833a7d0d11
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-08-27 17:14:07 +0530

    Implement register view

--- base/views.py
+++ base/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+import pymongo
+from decouple import config
+from .models import Users
+
+client = pymongo.MongoClient(config("HOST"))
+db = client['GuviTaskDB']['users']
 
 def home(request):
     return render(request, "home.html")
+
+def register(request):
+    if request.method == 'POST':        
+        content = {
+            'username': request.POST['username'],
+            'email': request.POST['email'],
+            'password': make_password(request.POST['password'])
+        }
+        db.insert_one(content)
+        return redirect('login')  
+    
+    return render(request, 'register.html')
