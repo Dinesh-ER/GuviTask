30bf5475ddf82ff71e04953956693092ba5c6c00
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-08-27 16:52:51 +0530

    Define users model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=50, primary_key=True)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=50)
+    contact = models.CharField(max_length=30, blank=False)
+    dob = models.CharField(max_length=12, blank=False)
+    age = models.IntegerField(null=True)
+    country = models.CharField(max_length=30, blank=False)
+    is_verified = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = "users"
